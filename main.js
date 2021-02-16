164ef3adb5eec8fc7452ddf1d5b74a9b5e150b39
marcopatrizi <64093578+marcopatrizi@users.noreply.github.com>
2021-02-16 10:14:37 +0100

    update

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1920,
-    height: 1080,
+    width: 550,
+    height: 700,
     frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
