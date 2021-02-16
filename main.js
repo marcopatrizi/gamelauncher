1f789f0f73ef37c8cfd8f16d234396077cf0ee2d
marcopatrizi <64093578+marcopatrizi@users.noreply.github.com>
2021-02-16 10:11:41 +0100

    update

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
+    frame: false,
     webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
   })
@@ -39,9 +41,6 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 
 function selezionaApp(){
